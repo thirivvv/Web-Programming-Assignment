a895f6025c89f446d827bc430053a3190182cc0a
Victor <poneponelop@gmail.com>
2025-07-25 10:22:50 -0700

    cmt by ThiriKyaw and Winstant

--- form.php
+++ form.php
@@ -13,7 +13,7 @@
 <body>
   <h2>Student Registration Form</h2>
 
-    <form action="process.php" method="POST" novalidate>
+  <form action="process.php" method="POST" novalidate>
     Student name:
     <input type="text" name="fname" placeholder="First Name">
     <input type="text" name="lname" placeholder="Last Name"><br><br>
@@ -30,7 +30,36 @@
     Mobile no.: +95-
     <input type="text" name="mobile"><br><br>
 
-   
+    Email:
+    <input type="email" name="email"><br><br>
+
+    Password:
+    <input type="password" name="password" pattern=".{8,}" title="Minimum 8 characters"><br><br>
+
+    Gender:
+    <input type="radio" name="gender" value="Male"> Male
+    <input type="radio" name="gender" value="Female"> Female<br><br>
+
+    Department:<br>
+    <input type="checkbox" name="department[]" value="English"> English
+    <input type="checkbox" name="department[]" value="Computer"> Computer
+    <input type="checkbox" name="department[]" value="Business"> Business<br><br>
+
+    Course:
+    <select name="course">
+      <option value="">Select Course</option>
+      <option value="Web Development">Web Development</option>
+      <option value="Networking">Networking</option>
+      <option value="Design">Design</option>
+    </select><br><br>
+
+    City:
+    <input type="text" name="city"><br><br>
+
+    Address:<br>
+    <textarea name="address" rows="3" cols="30"></textarea><br><br>
+
+    <input type="submit" name="submit" value="Register">
   </form>
 </body>
 </html>
