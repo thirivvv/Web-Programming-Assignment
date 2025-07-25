be1da9f518a8c29bc231f667b7f88286b4de93db
Victor <poneponelop@gmail.com>
2025-07-25 10:12:32 -0700

    vic 1st commit for changed file

--- form.php
+++ form.php
@@ -13,8 +13,22 @@
 <body>
   <h2>Student Registration Form</h2>
 
-  <form action="process.php" method="POST" novalidate>
-   
+    <form action="process.php" method="POST" novalidate>
+    Student name:
+    <input type="text" name="fname" placeholder="First Name">
+    <input type="text" name="lname" placeholder="Last Name"><br><br>
+
+    Father's name:
+    <input type="text" name="father"><br><br>
+
+    Date of birth:
+    <input type="text" name="day" placeholder="Day" size="2"> -
+    <input type="text" name="month" placeholder="Month" size="2"> -
+    <input type="text" name="year" placeholder="Year" size="4">
+    <span>(DD-MM-YYYY)</span><br><br>
+
+    Mobile no.: +95-
+    <input type="text" name="mobile"><br><br>
 
    
   </form>
